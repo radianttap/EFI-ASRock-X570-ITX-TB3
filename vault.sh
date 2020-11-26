85a660853b4bfc4d0a0e403e2b59620fa39aa7ce
Aleksandar Vacić <av@radianttap.com>
2020-11-26 17:26:58 +0100

    Remove this, will not use Vaulting

--- vault.sh
+++
@@ -1,6 +0,0 @@
-cd EFI/OC
-../../tools/CreateVault/create_vault.sh .
-../../tools/CreateVault/RsaTool -sign vault.plist vault.sig vault.pub 
-off=$(($(strings -a -t d OpenCore.efi | grep "=BEGIN OC VAULT=" | cut -f1 -d' ')+16))
-dd of=OpenCore.efi if=vault.pub bs=1 seek=$off count=528 conv=notrunc
-rm vault.pub
